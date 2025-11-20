28326e7e89721451440d8680c6f6f957775acac3
Wimers <113898263+Wimers@users.noreply.github.com>
2025-11-20 11:46:23 +1000

    Parses the DIB Header (assumes BITMAPINFOHEADER) and prints contents of the specified input file to terminal.

--- fileParsing.h
+++ fileParsing.h
@@ -6,6 +6,7 @@
 #define EXIT_INVALID_ARG 20
 #define EXIT_FILE_CANNOT_BE_READ 9
 #define EXIT_FILE_PARSING_ERROR 8
+#define EXIT_FILE_INTEGRITY 7
 
 // cmd line argument indexes
 #define FILE_PATH_IX 1
@@ -39,7 +40,17 @@
 const char* const fileOpeningErrorMessage
         = "The provided file \"%s\" cannot be opened for reading\n";
 const char* const invalidArgsMessage = "Invalid arguments supplied\n";
+const char* const invalidColourPlanesMessage
+        = "The number of colour planes must be 1, got \"%d\"\n";
 const char* const bitMap = "BM";
+const char* const lineSeparator
+        = "--------------------------------------------------\n";
+const char* const suXFormat = "%-25s %-15u %X\n";
+const char* const sdXFormat = "%-25s %-15d %X\n";
+const char* const sssFormat = "%-25s %-15s %s\n";
+const char* const ssdFormat = "%-25s %-15s %d\n";
+const char* const sudFormat = "%-25s %-15u %d\n";
+const char* const newlineStr = "\n";
 
 // Assorted constant chars
 const char* const readMode = "r";
@@ -51,6 +62,60 @@ typedef struct { // 14 bytes
     uint32_t offset;
 } BmpHeader;
 
-// Function prototypes
-void parse_bit_map_header(BmpHeader* bmp, FILE* file);
+// DIB header (bitmap information header)
+typedef struct { // 40 bytes
+    uint32_t headerSize;
+    int16_t bitmapWidth;
+    int16_t bitmapHeight;
+    uint16_t colourPlanes;
+    uint16_t bitsPerPixel;
+    uint32_t compression;
+    uint32_t imageSize;
+    uint32_t horzResolution;
+    uint32_t vertResolution;
+    uint32_t coloursInPalette;
+    uint32_t importantColours;
+} BmpInfoHeader;
+
+/* parse_bmp_header()
+ * ------------------
+ * Parses the provided file, and stores the associated bmp metadata,
+ * into the BMP header struct.
+ *
+ * It is assumed the input file is a .BMP file.
+ *
+ * bmp: Pointer to struct to store BMP Header metadata.
+ * file: BMP file to parse.
+ */
+void parse_bmp_header(BmpHeader* bmp, FILE* file);
+
+/* parse_bmp_info_header()
+ * -----------------------
+ * Parses the provided file, and stores the associated bmp metadata,
+ * into the BMP info header struct.
+ *
+ * It is assumed the input file is .BMP file.
+ *
+ * bmp: Pointer to struct to store BMP Info Header metadata.
+ * file: BMP file to parse.
+ *
+ * Errors: Exits with EXIT_FILE_INTEGRITY if the number of colour planes of file
+ *         is not one.
+ */
+void parse_bmp_info_header(BmpInfoHeader* bmp, FILE* file);
+
+/* print_bmp_header()
+ * ------------------
+ * Prints the values of each parameter from the BMP Info header parsed.
+ *
+ * bmp: Struct containing all parsed BMP Header metadata.
+ */
 void print_bmp_header(BmpHeader bmp);
+
+/* print_bmp_info_header()
+ * -----------------------
+ * Prints the values of each parameter from the BMP Info header parsed.
+ *
+ * bmp: Struct containing all parsed BMP Info Header metadata.
+ */
+void print_bmp_info_header(BmpInfoHeader bmp);
--- main.c
+++ main.c
@@ -25,15 +25,21 @@ int main(int argc, char** argv)
 
     BmpHeader bmp;
     memset(&bmp, 0, sizeof(bmp));
-    parse_bit_map_header(&bmp, file);
-    fclose(file);
+    parse_bmp_header(&bmp, file);
+
+    BmpInfoHeader infoHeader;
+    memset(&infoHeader, 0, sizeof(infoHeader));
+    parse_bmp_info_header(&infoHeader, file);
 
     print_bmp_header(bmp);
+    print_bmp_info_header(infoHeader);
+
+    fclose(file);
 
     exit(EXIT_OK);
 }
 
-void parse_bit_map_header(BmpHeader* bmp, FILE* file)
+void parse_bmp_header(BmpHeader* bmp, FILE* file)
 {
     // Store the value in the ID field
     fread(&(bmp->id), 2, 1, file);
@@ -46,11 +52,56 @@ void parse_bit_map_header(BmpHeader* bmp, FILE* file)
     fread(&(bmp->offset), 4, 1, file);
 }
 
+void parse_bmp_info_header(BmpInfoHeader* bmp, FILE* file)
+{
+    fseek(file, 14, SEEK_SET); // Seek to start of header
+
+    fread(&bmp->headerSize, 4, 1, file); // Header size in bytes
+    fread(&bmp->bitmapWidth, 4, 1, file); // Bitmap width in pixles
+    fread(&bmp->bitmapHeight, 4, 1, file); // Bitmap height in pixels
+    fread(&bmp->colourPlanes, 2, 1, file); // Number of colour planes
+
+    if ((int)(bmp->colourPlanes) != 1) { // Must be one
+        fprintf(stderr, invalidColourPlanesMessage, (int)(bmp->colourPlanes));
+        exit(EXIT_FILE_INTEGRITY);
+    }
+
+    fread(&bmp->bitsPerPixel, 2, 1, file); // Image colour depth (i.e. 16, 24)
+    fread(&bmp->compression, 4, 1, file); // BI_RGB (no compression) most common
+    fread(&bmp->imageSize, 4, 1, file); // Size of the raw bitmap data
+    fread(&bmp->horzResolution, 4, 1, file);
+    fread(&bmp->vertResolution, 4, 1, file);
+    fread(&bmp->coloursInPalette, 4, 1, file);
+    fread(&bmp->importantColours, 4, 1, file); // 0 if all colours important
+}
+
 void print_bmp_header(BmpHeader bmp)
 {
-    fprintf(stdout, "%-15s %-15s %s\n", "Field Name", "Data", "Hex");
-    fprintf(stdout, "---------------------------------------\n");
-    fprintf(stdout, "%-15s %-15s %d\n", "ID", (char*)&(bmp.id), bmp.id);
-    fprintf(stdout, "%-15s %-15u %X\n", "Size", bmp.bmpSize, bmp.bmpSize);
-    fprintf(stdout, "%-15s %-15u %X\n", "Offset", bmp.offset, bmp.offset);
+    fprintf(stdout, sssFormat, "BMP Header", "Data", "Hex");
+    fputs(lineSeparator, stdout);
+    fprintf(stdout, ssdFormat, "ID", (char*)&(bmp.id), bmp.id);
+    fprintf(stdout, suXFormat, "Size", bmp.bmpSize, bmp.bmpSize);
+    fprintf(stdout, suXFormat, "Offset", bmp.offset, bmp.offset);
+}
+
+void print_bmp_info_header(BmpInfoHeader bmp)
+{
+    fputs(newlineStr, stdout);
+    printf(sssFormat, "DIB Header", "Data", "Hex");
+    fputs(lineSeparator, stdout);
+    printf(suXFormat, "Header Size", bmp.headerSize, bmp.headerSize);
+    printf(sdXFormat, "Bitmap Width", bmp.bitmapWidth, bmp.bitmapWidth);
+    printf(sdXFormat, "Bitmap Height", bmp.bitmapHeight, bmp.bitmapHeight);
+    printf(sudFormat, "Num. Colour Planes", bmp.colourPlanes, bmp.colourPlanes);
+    printf(sudFormat, "Bits Per Pixel", bmp.bitsPerPixel, bmp.bitsPerPixel);
+    printf(suXFormat, "Compression", bmp.compression, bmp.compression);
+    printf(suXFormat, "Image Size", bmp.imageSize, bmp.imageSize);
+    printf(sdXFormat, "Horizontal Resolution", bmp.horzResolution,
+            bmp.horzResolution);
+    printf(sdXFormat, "Verticle Resolution", bmp.vertResolution,
+            bmp.vertResolution);
+    printf(suXFormat, "Colours In Palette", bmp.coloursInPalette,
+            bmp.coloursInPalette);
+    printf(suXFormat, "Important Colours", bmp.importantColours,
+            bmp.importantColours);
 }
